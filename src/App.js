0f590b78223309e590851663fddb5104cddaaead
takazo3 <info@re-zero-jp.com>
2023-08-07 02:23:58 +0900

    04 Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import ESignatureApp from './Projects/01-e-signature-app/ESignatureApp';
 import RandamaizColors from './Projects/02-randomize-colors/RandamaizColors';
-import LikePhotoApp from './Projects/3-like-my-photo/LikePhotoApp';
+import LikePhotoApp from './Projects/03-like-my-photo/LikePhotoApp';
+import Testimonial from './Projects/04-testimonials-app/Testimonial';
+
 
 function App() {
   return (
     <div className="App">
       {/* <ESignatureApp /> */}
       {/* <RandamaizColors /> */}
-      <LikePhotoApp />
+      {/* <LikePhotoApp /> */}
+      <Testimonial />
+
 
     </div>
   );
