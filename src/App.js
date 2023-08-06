4fcdb547938dce27470b6f42d19c2f1e5584899b
takazo3 <info@re-zero-jp.com>
2023-08-07 03:54:52 +0900

    05 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ESignatureApp from './Projects/01-e-signature-app/ESignatureApp';
 import RandamaizColors from './Projects/02-randomize-colors/RandamaizColors';
 import LikePhotoApp from './Projects/03-like-my-photo/LikePhotoApp';
 import Testimonial from './Projects/04-testimonials-app/Testimonial';
+import AlertApp from './Projects/05-alert/AlertApp';
+
 
 
 function App() {
@@ -10,9 +12,9 @@ function App() {
     <div className="App">
       {/* <ESignatureApp /> */}
       {/* <RandamaizColors /> */}
-      {/* <LikePhotoApp /> */}
-      <Testimonial />
-
+      {/* <LikePhotoApp />
+      <Testimonial /> */}
+      <AlertApp />
 
     </div>
   );
---
+++ src/Projects/05-alert/AlertApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Alert from '../components/Alert'
+
+export default function AlertApp() {
+  return (
+    <div className="container mx-auto">
+      <Alert type="success" message={"Successful login!"} />
+      <Alert type="info" message={"Time triggered!"} delay={true} delayTime={1000}/>
+    </div>
+  )
+}
