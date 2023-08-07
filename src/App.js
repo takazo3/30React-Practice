99a0e71eed5fa6852ca9471dc53029f2f5178cf9
takazo3 <info@re-zero-jp.com>
2023-08-08 00:40:23 +0900

    07Light and dark mode  ※

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LikePhotoApp from './Projects/03-like-my-photo/LikePhotoApp';
 import Testimonial from './Projects/04-testimonials-app/Testimonial';
 import AlertApp from './Projects/05-alert/AlertApp';
 import TemperatureControllerApp from './Projects/06-temperature-controller/TemperatureControllerApp';
+import LightDarkMode from './Projects/07-light-dark-mode/LightDarkMode';
 
 
 
@@ -16,7 +17,8 @@ function App() {
       {/* <LikePhotoApp />
       <Testimonial /> */}
       {/* <AlertApp /> */}
-      <TemperatureControllerApp />
+      {/* <TemperatureControllerApp /> */}
+      <LightDarkMode />
 
     </div>
   );
