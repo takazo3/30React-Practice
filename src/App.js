cf2ba8e9875a7b5f10c139285b39bca779a3872a
takazo3 <info@re-zero-jp.com>
2023-08-07 23:20:33 +0900

    06-tempControllir

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RandamaizColors from './Projects/02-randomize-colors/RandamaizColors';
 import LikePhotoApp from './Projects/03-like-my-photo/LikePhotoApp';
 import Testimonial from './Projects/04-testimonials-app/Testimonial';
 import AlertApp from './Projects/05-alert/AlertApp';
+import TemperatureControllerApp from './Projects/06-temperature-controller/TemperatureControllerApp';
 
 
 
@@ -14,7 +15,8 @@ function App() {
       {/* <RandamaizColors /> */}
       {/* <LikePhotoApp />
       <Testimonial /> */}
-      <AlertApp />
+      {/* <AlertApp /> */}
+      <TemperatureControllerApp />
 
     </div>
   );
